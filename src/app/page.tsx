e53ad7f131261b78e4c5ba6dc09074c2e7963361
Lucas Conto <lcdeksb@gmail.com>
2026-09-03 18:24:41 -0300

    fix_smart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,9 +85,6 @@ export default function DashboardPage() {
       {/* Metric Cards */}
       <MetricCards viewMode={viewMode} data={metricData} />
 
-      {/* Smart Alerts */}
-      <SmartAlerts />
-
       {/* Main Charts Row */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
